74a90d25657527ef88beaba8047a777a4dc8526a
ShaileshVSavani <svs.rnw23@gmail.com>
2024-11-24 06:17:58 +0530

    first

--- src/constant.js
+++ src/constant.js
@@ -1,10 +1,3 @@
 
-// // const API_BASE_URL = "http://localhost:5000";
-// const API_BASE_URL =
-//   "https://kitchen-recipe-management-backend-fpt3.onrender.com";
-
-// export default API_BASE_URL;
-
-
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export default API_BASE_URL;
